bfa2de2ea4e082e928ebdcc5386241c5da131efc
Abdulganiu Abdulrahmon <abdulrahmonabdulganiy@gmail.com>
2026-05-03 23:32:20 +0100

    fix: fixed smtp issues

--- src/email/mailer.js
+++ src/email/mailer.js
@@ -4,8 +4,8 @@ require("dotenv").config();
 const transporter = nodemailer.createTransport({
   pool: true,
   host: process.env.MAIL_HOST,
-  /* port: 587 */ port: process.env.MAIL_PORT,
-  /* secure: false  */ secure: true,
+  port: 587 /* port: process.env.MAIL_PORT */,
+  secure: false /* secure: true */,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
