9d49c60c3766869306f886b8a4ed38daf3b2708b
Abdulganiu Abdulrahmon <abdulrahmonabdulganiy@gmail.com>
2026-05-03 23:44:34 +0100

    fixed mail timeout issues

--- src/email/mailer.js
+++ src/email/mailer.js
@@ -3,9 +3,9 @@ require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
   pool: true,
-  host: process.env.MAIL_HOST,
-  port: 587 /* port: process.env.MAIL_PORT */,
-  secure: false /* secure: true */,
+  host: "smtp.gmail.com",
+  /* port: 587 */ port: process.env.MAIL_PORT,
+  /* secure: false */ secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
