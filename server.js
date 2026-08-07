375241891ad062d379c2cf90ce8b254e11c4bff0
Abdulganiu Abdulrahmon <abdulrahmonabdulganiy@gmail.com>
2026-08-07 22:16:27 +0100

    fixed CORS issue on live vercel link

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(
     origin: [
       "http://localhost:5174",
       "http://localhost:5173",
-      "https://fintech-client-snowy.vercel.app/",
+      "https://fintech-client-snowy.vercel.app",
     ],
     credentials: true,
   }),
